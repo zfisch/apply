ea421c39d63b7c4387e7375e187bf96247cf55f1
Zack Fischmann <zacharyrobert@gmail.com>
2015-07-08 08:41:37 -0700

    serve index.html at route path

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Job app!');
-});
+app.use(express.static(__dirname + '/client'));
+
+// app.get('/', function(req, res) {
+//   res.send('Job app!');
+// });
 
 var db = require('./server/db.js');
 
